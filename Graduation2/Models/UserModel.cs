e4e8ef7c23cf59e23296e3cfe494b441d0247877
unknown <dsaf2007@dongguk.edu>
2022-01-26 20:47:39 +0900

    getRule method

--- Graduation2/Models/UserModel.cs
+++ Graduation2/Models/UserModel.cs
@@ -8,11 +8,55 @@ using System.Threading.Tasks;
 using System.Collections.Generic;
 using ExcelDataReader;
 using MySql.Data.MySqlClient;
+//using Rule.Models;
 
 namespace Graduation2.Models
 {
+  public class TempRule
+  {
+    // TempRule(){}
+    // TempRule(string keyword_, string question_type_, string value_)
+    // {
+    //   this.keyword = keyword_;
+    //   this.question_type = question_type_;
+    //   this.value = value_;
+    // }
+    public string keyword;
+    public string question_type;
+    public string value;
+  }
   public class UserInfo // 취득교과목
   {
+
+    //public List<Rule.Models.Rule> rule = new List<Rule.Models.Rule>();
+    public List<TempRule> rule = new List<TempRule>();
+
+    public void getRule()
+  {
+    using (MySqlConnection connection  = new MySqlConnection("Server=101.101.216.163/;Port=5555;Database=testDB;Uid=CSDC;Pwd=1q2w3e4r"))
+    {
+      string selectQuery = "SELECT * FROM rule";
+      connection.Open();
+      MySqlCommand command = new MySqlCommand(selectQuery, connection);
+
+      using (var reader = command.ExecuteReader())
+      {
+        while(reader.Read())
+        {
+          TempRule temp = new TempRule();
+          temp.keyword=reader["keyword"].ToString();
+          temp.question_type=reader["question_type"].ToString();
+          temp.value = reader["value"].ToString();
+          //rule.Add(new TempRule(){reader["keyword"].ToString(),reader["question_type"].ToString(),reader["value"].ToString()});
+          rule.Add(temp);
+
+          
+        }
+      }
+    }
+  }
       
   }
+
+
 }
